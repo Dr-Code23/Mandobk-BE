eb284f35ae2e6bb3c64eaf2819968a4e98adc154
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-01-24 22:20:58 +0200

    Update

--- app/Http/Controllers/Api/Web/V1/Auth/webAuthController.php
+++ app/Http/Controllers/Api/Web/V1/Auth/webAuthController.php
@@ -1,18 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Api\Web\V1;
+namespace App\Http\Controllers\Api\Web\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Web\V1\Auth\webLoginRequest as AuthWebLoginRequest;
 use App\Http\Requests\Api\Web\V1\Auth\webSignUpRequest as AuthWebSignUpRequest;
-use App\Http\Requests\Api\Web\V1\webSignUpRequest;
-use App\Http\Requests\Api\Web\V1\webLoginRequest;
 use App\Models\User;
 use App\Traits\HttpResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 use Symfony\Component\HttpFoundation\Response;
 
 class webAuthController extends Controller
@@ -20,41 +16,59 @@ class webAuthController extends Controller
     use HttpResponse;
 
     /**
-     * Register A New User
-     * @param AuthWebSignUpRequest $req
+     * Register A New User.
+     *
      * @return \Illuminate\Http\JsonResponse
      */
-
     public function signup(AuthWebSignUpRequest $req)
     {
         User::create([
             'full_name' => $req->full_name,
             'username' => $req->username,
-            'password'=> Hash::make($req->password),
+            'password' => Hash::make($req->password),
             'phone' => $req->phone,
-            'role' => $req->role
+            'role' => $req->role,
         ]);
-        $token = Auth::attempt(['username' => $req->username , 'password' => $req->password]);
-        $jwt_cookie = cookie('jwt_token' , $token , 60);
-        return $this->responseWithCookie($jwt_cookie,null, 'Account Created Successfully , and user logged in');
+        $token = Auth::attempt(['username' => $req->username, 'password' => $req->password]);
+        $jwt_cookie = cookie('jwt_token', $token, 60);
+
+        return $this->responseWithCookie($jwt_cookie, null, 'Account Created Successfully , and user logged in');
     }
 
-    public function login(AuthWebLoginRequest $req){
-
+    /**
+     * Login User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(AuthWebLoginRequest $req)
+    {
         // Get Credentials
         $username = htmlspecialchars($req->username);
         $password = htmlspecialchars($req->password);
 
         // Check if the user exists
-        if($token = Auth::attempt(['username' => $username , 'password' => $password])){
+        if ($token = Auth::attempt(['username' => $username, 'password' => $password])) {
             $user = Auth::user();
-            $jwt_cookie = cookie('jwt_token' , $token , 60);
+            $jwt_cookie = cookie('jwt_token', $token, 60);
+
             return $this->responseWithCookie($jwt_cookie, [
                 'username' => $user->username,
                 'full_name' => $user->full_name,
                 'role' => $user->role,
-            ] , 'User Authenticated Successfully');
-        } else
+            ], 'User Authenticated Successfully');
+        } else {
             return $this->unauthenticatedResponse('You are not authorized', Response::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    /**
+     * Logout User
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        Auth::logout();
+
+        return $this->success(msg: 'User Logged out successfully');
     }
 }
--- routes/api/v1/web.php
+++ routes/api/v1/web.php
@@ -1,9 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => '\App\Http\Controllers\Api\Web\V1'], function () {
+Route::group(['namespace' => '\App\Http\Controllers\Api\Web\V1\Auth'], function () {
     Route::post('/signup', 'webAuthController@signup')->name('add-user');
     Route::post('/login', 'webAuthController@login')->name('login-user');
+    Route::post('/logout', 'webAuthController@logout')->name('web-logout')
+        ->middleware('auth:api');
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
 });
