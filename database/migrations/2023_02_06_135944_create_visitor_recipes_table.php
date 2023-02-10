dd0c9b44d90837f9fd1d7d49c7f6a86dcfc910bc
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-02-10 15:08:01 +0200

    update default value

--- database/migrations/2023_02_06_135944_create_visitor_recipes_table.php
+++ database/migrations/2023_02_06_135944_create_visitor_recipes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
@@ -24,7 +23,7 @@ return new class() extends Migration
                 ->cascadeOnDelete();
             $table->string('random_number')->unique();
             $table->string('alias');
-            $table->json('details')->default(json_encode([]));
+            $table->json('details');
             $table->timestamps();
         });
     }
