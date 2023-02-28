a00e4b7668632f6ca88030a670b624d0c79afe02
Mohamed Attar <mohammedattar0100020@gmail.com>
2023-02-28 14:58:32 +0200

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,1 @@
-<script src="{{ asset('build/assets/app-23a62441.js') }}"></script>
-
-<script>
-    Echo.private('google').listen('TestEvent', function(e) {
-        console.log('Your Message Is ', e.message)
-    })
-
-    Echo.private('newUserRegister').listen('RegisterUserEvent', function(e) {
-        console.log('New User Registred , His Data is ' + e.payload.data)
-        console.log('New User Registred , His Data is ', e.payload)
-    })
-</script>
+Hello Mandobk
